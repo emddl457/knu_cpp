6cd3c21f8c3e4475ec821e6d2aa9b0f2bb489712
emddl457 <emddl456@gmail.com>
2025-05-14 20:08:12 +0900

    8장 8번 실습문제

--- Chap8_8/8-8.cpp
+++ Chap8_8/8-8.cpp
@@ -2,107 +2,111 @@
 #include <string>
 using namespace std;
 
-// 프린터의 기본 클래스
+// 프린터 기본 클래스
 class Printer {
 protected:
-    string model;        // 모델명
-    string manufacturer; // 제조사
-    int printedCount;    // 인쇄된 총 페이지 수
+    string model;
+    string manufacturer;
+    int printedCount;
+    int availableCount;
 public:
-    Printer(string model, string manufacturer) {
+    Printer(string model, string manufacturer, int availableCount) {
         this->model = model;
         this->manufacturer = manufacturer;
+        this->availableCount = availableCount;
         this->printedCount = 0;
     }
 
-    // 가상함수로 인쇄 기능 정의 (자식 클래스에서 오버라이딩)
-    virtual void print(int pages) = 0;
-
-    // 프린터 정보 출력 함수
+    virtual void print(int pages) = 0; // 순수가상함수
     virtual void show() = 0;
 };
 
 // 잉크젯 프린터 클래스
 class InkJetPrinter : public Printer {
-    int availableInk;  // 남은 잉크량
+    int availableInk;
 public:
-    InkJetPrinter(string model, string manufacturer, int availableInk)
-        : Printer(model, manufacturer) {
+    InkJetPrinter(string model, string manufacturer, int availableCount, int availableInk)
+        : Printer(model, manufacturer, availableCount) {
         this->availableInk = availableInk;
     }
 
     void print(int pages) override {
-        if (availableInk >= pages) {
+        if (availableCount >= pages && availableInk >= pages) {
+            availableCount -= pages;
             availableInk -= pages;
             printedCount += pages;
             cout << "프린트하였습니다." << endl;
         }
         else {
-            cout << "잉크가 부족하여 프린트할 수 없습니다." << endl;
+            cout << "용지나 잉크가 부족하여 프린트할 수 없습니다." << endl;
         }
     }
 
     void show() override {
-        cout << model << " / " << manufacturer << " / 남은 종이 " << (100 - printedCount)
-            << " / 남은 잉크 " << availableInk << endl;
+        cout << model << " , " << manufacturer << " , 남은 종이 "
+            << availableCount << "장 , 남은 잉크 " << availableInk << endl;
     }
 };
 
 // 레이저 프린터 클래스
 class LaserPrinter : public Printer {
-    int availableToner;  // 남은 토너량
+    int availableToner;
 public:
-    LaserPrinter(string model, string manufacturer, int availableToner)
-        : Printer(model, manufacturer) {
+    LaserPrinter(string model, string manufacturer, int availableCount, int availableToner)
+        : Printer(model, manufacturer, availableCount) {
         this->availableToner = availableToner;
     }
 
     void print(int pages) override {
-        if (availableToner >= pages) {
+        if (availableCount >= pages && availableToner >= pages) {
+            availableCount -= pages;
             availableToner -= pages;
             printedCount += pages;
             cout << "프린트하였습니다." << endl;
         }
         else {
-            cout << "토너가 부족하여 프린트할 수 없습니다." << endl;
+            cout << "용지나 토너가 부족하여 프린트할 수 없습니다." << endl;
         }
     }
 
     void show() override {
-        cout << model << " / " << manufacturer << " / 남은 종이 " << (100 - printedCount)
-            << " / 남은 토너 " << availableToner << endl;
+        cout << model << " , " << manufacturer << " , 남은 종이 "
+            << availableCount << "장 , 남은 토너 " << availableToner << endl;
     }
 };
 
 int main() {
-    // 프린터 객체 배열 (잉크젯과 레이저)
-    InkJetPrinter ink("Officejet V40", "HP", 10);
-    LaserPrinter laser("SCX-6x45", "삼성전자", 20);
+    InkJetPrinter ink("Officejet V40", "HP", 5, 10);
+    LaserPrinter laser("SCX-6x45", "삼성전자", 3, 20);
+
+    // 상태 출력은 한 번만
+    cout << "현재 작동중인 2 대의 프린터는 아래와 같다" << endl;
+    cout << "잉크젯 : "; ink.show();
+    cout << "레이저 : "; laser.show();
 
     while (true) {
-        cout << "\n현재 작동중인 2 대의 프린터는 아래와 같다" << endl;
-        cout << "잉크젯 : "; ink.show();
-        cout << "레이저 : "; laser.show();
-
-        cout << "프린터(1:잉크젯, 2:레이저)와 매수 입력>> ";
+        cout << "\n프린터(1:잉크젯, 2:레이저)와 매수 입력>> ";
         int type, pages;
         cin >> type >> pages;
 
-        if (type == 1) {
+        if (type == 1)
             ink.print(pages);
-        }
-        else if (type == 2) {
+        else if (type == 2)
             laser.print(pages);
-        }
-        else {
-            cout << "잘못된 입력입니다." << endl;
-        }
+        else
+            cout << "잘못된 선택입니다." << endl;
+
+        // 현재 남은 잉크/토너/종이 상태만 반복 출력
+        ink.show();
+        laser.show();
 
         cout << "계속 프린트 하시겠습니까?(y/n)>> ";
         char cont;
         cin >> cont;
-        if (cont == 'n' || cont == 'N') break;
+        if (cont == 'n' || cont == 'N')
+            break;
     }
 
     return 0;
 }
+
